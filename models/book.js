ae0709e794ae38efd2b240b282ca8fca67910a5a
johndolac <johndolacjr@gmail.com>
2021-02-21 20:36:41 -0700

    cleaned up some code

--- models/book.js
+++ models/book.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   authors: { type: String, required: true },
-  description: String,
+  description: {type: String, required: true},
   image: { type: Date, default: Date.now },
-  link: String
+  link: {type: String, required: false}
 });
 
 const Book = mongoose.model("Book", bookSchema);
